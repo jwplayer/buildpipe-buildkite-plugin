4e7d70152406f0fd0ecdc5ef38f8cc07c094ed30
Moritz Wiesinger <momo201193@gmail.com>
2020-02-06 23:29:37 +0100

    Change pipeline steps if buildkite trigger step is used (#41)
    * update gitignore
    
    * add function to check for buildkite trigger steps
    
    * Update steps in-place, unit test
    
    * adjust testcase
    
    * adjust testcase

--- buildpipe/pipeline.py
+++ buildpipe/pipeline.py
@@ -236,6 +236,14 @@ def iter_stair_projects(stair: box.Box, projects: Set[box.Box]) -> Generator[box
             yield project
 
 
+def check_for_trigger_steps(build_steps):
+    for step in build_steps:
+        if 'trigger' in step:
+            step['build'] = {}
+            step['build']['env'] = step['env']
+            del step['env']
+
+
 def compile_steps(config: box.Box) -> box.Box:
     validate_config(config)
     branch = get_git_branch()
@@ -251,6 +259,7 @@ def compile_steps(config: box.Box) -> box.Box:
             steps += generate_wait_step(previous_stair)
             steps += generate_block_step(config.block.to_dict(), stair, stair_projects)
             steps += scope_fn[stair.scope](stair, stair_projects)
+        check_for_trigger_steps(steps)
         previous_stair = stair
 
     return box.Box({'steps': steps})
