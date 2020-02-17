870d4b466fab3f469343f808d1a6562b25e5ff95
Guilhem Charles <gcharles@jwplayer.com>
2020-02-17 18:14:49 +0100

    add CLI parameter to get inferred version: -V or --version

--- buildpipe/__main__.py
+++ buildpipe/__main__.py
@@ -1,10 +1,11 @@
 import argparse
 
-from buildpipe import pipeline
+from buildpipe import pipeline, __version__
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
+    parser.add_argument('--version', '-V', action='version', version=__version__)
     parser.add_argument('--infile', '-i', default='buildpipe.yml', metavar='FILE', help='Configuration file')
     parser.add_argument('--outfile', '-o', default='pipeline.yml', help='File for uploading to Buildkite')
     parser.add_argument('--dry-run', action='store_true', help='Validate infile')
